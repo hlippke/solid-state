f500d90791bdbc270feb180d16ac87c58b29ebe7
bighatsmallfeet@gmail.com <bighatsmallfeet@gmail.com@e61a49ff-851c-bdff-0f10-69646a315ee7>
2013-03-16 17:28:28 +0000

    Added test project and configuration tests

---
+++ trunk/Solid.State.Tests/StateMachineTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solid.State.Tests.TelephoneStates;
+
+namespace Solid.State.Tests
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        // Private variables
+
+        private enum TelephoneTrigger
+        {
+            PickingUpPhone,
+            IncomingCall,
+            Timeout
+        }
+
+        private SolidMachine<TelephoneTrigger> BuildTelephoneStateMachine()
+        {
+            var sm = new SolidMachine<TelephoneTrigger>();
+
+            sm.State<IdleState>()
+                .IsInitialState()
+                .On(TelephoneTrigger.PickingUpPhone).GoesTo<DiallingState>()
+                .On(TelephoneTrigger.IncomingCall).GoesTo<RingingState>();
+
+            sm.State<RingingState>()
+                .On(TelephoneTrigger.PickingUpPhone).GoesTo<ConversationState>()
+                .On(TelephoneTrigger.Timeout).GoesTo<IdleState>();
+
+            sm.Start();
+
+            return sm;
+        }
+
+        /// <summary>
+        /// Tests that a state cannot have multiple unguarded transitions with the
+        /// same trigger.
+        /// </summary>
+        [TestMethod]
+        public void ConfigureMultipleUnguardedTriggers()
+        {
+            var sm = new SolidMachine<TelephoneTrigger>();
+
+            try
+            {
+                sm.State<IdleState>()
+                    .On(TelephoneTrigger.PickingUpPhone).GoesTo<DiallingState>()
+                    .On(TelephoneTrigger.PickingUpPhone).GoesTo<RingingState>();
+
+                Assert.Fail("Invalid configuration was permitted!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.IsTrue(ex is SolidStateException);
+            }
+        }
+
+        [TestMethod]
+        public void ConfigureUnguardedAndGuardedTrigger()
+        {
+            var sm = new SolidMachine<TelephoneTrigger>();
+
+            try
+            {
+                sm.State<IdleState>()
+                    .On(TelephoneTrigger.PickingUpPhone).GoesTo<DiallingState>()
+                    .On(TelephoneTrigger.PickingUpPhone, () => true).GoesTo<RingingState>();
+
+                Assert.Fail("Invalid configuration was permitted!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.IsTrue(ex is SolidStateException);
+            }
+        }
+
+        [TestMethod]
+        public void ConfigureMultipleInitialStates()
+        {
+            try
+            {
+                var sm = new SolidMachine<TelephoneTrigger>();
+
+                sm.State<IdleState>()
+                    .IsInitialState()
+                    .On(TelephoneTrigger.PickingUpPhone).GoesTo<DiallingState>()
+                    .On(TelephoneTrigger.IncomingCall).GoesTo<RingingState>();
+
+                sm.State<RingingState>()
+                    .IsInitialState()
+                    .On(TelephoneTrigger.PickingUpPhone).GoesTo<ConversationState>()
+                    .On(TelephoneTrigger.Timeout).GoesTo<IdleState>();
+
+                Assert.Fail("Invalid configuration was permitted!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.IsTrue(ex is SolidStateException);
+            }
+        }
+    }
+}
--- trunk/Solid.State/SolidMachine.cs
+++ trunk/Solid.State/SolidMachine.cs
@@ -5,7 +5,10 @@ namespace Solid.State
 {
     public partial class SolidMachine<TTrigger>
     {
-        // Private variables
+        // Variables
+
+        private StateConfiguration _initialState;
+        private bool _initialStateConfigured;
 
         internal Dictionary<Type, StateConfiguration> _stateConfigurations;
 
@@ -29,13 +32,49 @@ namespace Solid.State
             else
             {
                 configuration = new StateConfiguration(type, this);
+
+                // If this is the first state that is added, it becomes the initial state
+                if (_stateConfigurations.Count == 0)
+                    _initialState = configuration;
+
                 _stateConfigurations.Add(type, configuration);
             }
 
             return configuration;
         }
 
+        /// <summary>
+        /// Starts the state machine by going to the initial state.
+        /// </summary>
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
         // Properties
+
+        /// <summary>
+        /// Indicates if the initial state of the state machine has been configured yet.
+        /// It can only be configured once.
+        /// </summary>
+        internal bool InitialStateConfigured
+        {
+            get { return _initialStateConfigured; }
+        }
+
+        internal void SetInitialState(StateConfiguration initialStateConfiguration)
+        {
+            _initialState = initialStateConfiguration;
+            _initialStateConfigured = true;
+        }
+
+        /// <summary>
+        /// The type that is the initial state.
+        /// </summary>
+        internal Type InitialState
+        {
+            get { return _initialState.StateType; }
+        }
 
         public Dictionary<Type, StateConfiguration> StateConfigurations
         {
