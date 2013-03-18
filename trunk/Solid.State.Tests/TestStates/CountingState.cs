f854bfbe0d9334f3a2622b5d12a5237b362f5384
bighatsmallfeet@gmail.com <bighatsmallfeet@gmail.com@e61a49ff-851c-bdff-0f10-69646a315ee7>
2013-03-18 20:27:34 +0000

    Updated tests

---
+++ trunk/Solid.State.Tests/TestStates/CountingState.cs
@@ -0,0 +1,18 @@
+﻿namespace Solid.State.Tests.TestStates
+{
+    public class CountingState : SolidState
+    {
+        public override void Entering(object context)
+        {
+            // Increase the number on the state machines EnteringCount
+            var machine = context as TestStateMachine;
+            machine.EnteringCount++;
+        }
+
+        public override void Exiting(object context)
+        {
+            var machine = context as TestStateMachine;
+            machine.ExitingCount++;
+        }
+    }
+}
