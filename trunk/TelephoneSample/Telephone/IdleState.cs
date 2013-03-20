d5a2258187cc6c2bf0d1db093dd53e2b9b774c33
bighatsmallfeet@gmail.com <bighatsmallfeet@gmail.com@e61a49ff-851c-bdff-0f10-69646a315ee7>
2013-03-20 20:20:30 +0000

    Work on TelephoneSample

--- trunk/TelephoneSample/Telephone/IdleState.cs
+++ trunk/TelephoneSample/Telephone/IdleState.cs
@@ -4,5 +4,23 @@ namespace TelephoneSample.Telephone
 {
     public class IdleState : TelephoneState
     {
+        // Private variables
+
+        private ISimpleLog _log;
+
+        // Constructor
+
+        public IdleState(ISimpleLog log)
+        {
+            _log = log;
+        }
+
+        // Protected methods
+
+        protected override void DoEntering(SolidMachine<TelephoneTrigger> machine)
+        {
+            // Log that we're here
+            _log.Write("Telephone is idle\r\n");
+        }
     }
 }
