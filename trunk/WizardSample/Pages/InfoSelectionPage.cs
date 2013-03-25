566dafe35f2f26728fb1bc79d9bf80e71313b461
bighatsmallfeet@gmail.com <bighatsmallfeet@gmail.com@e61a49ff-851c-bdff-0f10-69646a315ee7>
2013-03-25 10:55:27 +0000

    Work on WizardSample

---
+++ trunk/WizardSample/Pages/InfoSelectionPage.cs
@@ -0,0 +1,34 @@
+﻿namespace WizardSample.Pages
+{
+    public partial class InfoSelectionPage : BasePage
+    {
+        // Constructor
+
+        public InfoSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        // Methods (BasePage)
+
+        protected override void DoEntering(WizardContext context)
+        {
+            // Read from context which radio button should be selected
+            radioFamily.Checked = context.InfoSelection == InfoSelectionMode.Family;
+            radioHome.Checked = context.InfoSelection == InfoSelectionMode.Home;
+            radioWork.Checked = context.InfoSelection == InfoSelectionMode.Work;
+        }
+
+        protected override void DoExiting(WizardContext context)
+        {
+            // Save the selected choice to context since it will be used in the state machine guard
+            // clauses when selecting the next page
+            if (radioFamily.Checked)
+                context.InfoSelection = InfoSelectionMode.Family;
+            else if (radioHome.Checked)
+                context.InfoSelection = InfoSelectionMode.Home;
+            else
+                context.InfoSelection = InfoSelectionMode.Work;
+        }
+    }
+}
---
+++ trunk/WizardSample/WizardContext.cs
@@ -0,0 +1,20 @@
+﻿namespace WizardSample
+{
+    /// <summary>
+    /// Holds information about the choices that are made in the wizard
+    /// and the data that is entered.
+    /// </summary>
+    public class WizardContext
+    {
+        // Properties
+
+        public InfoSelectionMode InfoSelection { get; set; }
+    }
+
+    public enum InfoSelectionMode
+    {
+        Family = 0,
+        Home = 1,
+        Work = 2
+    }
+}
