9640928c9a53528884ea7a5fbb6a3baacd9d6703
bighatsmallfeet@gmail.com <bighatsmallfeet@gmail.com@e61a49ff-851c-bdff-0f10-69646a315ee7>
2013-04-05 22:55:18 +0000

    Work on parallel states

---
+++ branches/parallell_states/Solid.State.Tests/TestStates/ParaState.cs
@@ -0,0 +1,195 @@
+﻿using System.Threading;
+
+namespace Solid.State.Tests.TestStates
+{
+    public abstract class ParaState : ISolidState
+    {
+        // Private variables
+
+        private Timer _timer;
+
+        /// <summary>
+        /// Must be overridden to provide a trigger that should be fired to exit this state.
+        /// </summary>
+        protected abstract ParallelTrigger GetExitTrigger();
+
+        /// <summary>
+        /// Must be overridden to provide a duration that the state should last.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract int GetDuration();
+
+        // Methods (ISolidState)
+
+        public void Entering(object context)
+        {
+            _timer = new Timer((callbackState) =>
+                {
+                    (context as SolidMachine<ParallelTrigger>).Trigger(GetExitTrigger());
+                }, null, GetDuration(), Timeout.Infinite);
+        }
+
+        public void Exiting(object context)
+        {
+            _timer.Dispose();
+        }
+    }
+
+    public enum ParallelTrigger
+    {
+        State1Ended,
+        State2Ended,
+        State2EndedDetour,
+        State3_1Ended,
+        State3_2Ended,
+        State3_3Ended,
+        State4_1Ended,
+        State4_2Ended,
+        State5_1Ended,
+        State5_2_1Ended,
+        State5_2_2Ended
+    }
+
+    public class ParaState1 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State1Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 100;
+        }
+    }
+
+    public class ParaState2 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State2Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 100;
+        }
+    }
+
+    public class ParaState3_1 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State3_1Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 300;
+        }
+    }
+
+    public class ParaState3_2 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State3_2Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 200;
+        }
+    }
+
+    public class ParaState3_3 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State3_3Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 100;
+        }
+    }
+
+    public class ParaState4_1 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State4_1Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 100;
+        }
+    }
+
+    public class ParaState4_2 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State4_2Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 300;
+        }
+    }
+
+    public class ParaState5_1 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State5_1Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 300;
+        }
+    }
+
+    public class ParaState5_2_1 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State5_2_1Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 300;
+        }
+    }
+
+    public class ParaState5_2_2 : ParaState
+    {
+        protected override ParallelTrigger GetExitTrigger()
+        {
+            return ParallelTrigger.State5_2_2Ended;
+        }
+
+        protected override int GetDuration()
+        {
+            return 100;
+        }
+    }
+
+    public class ParaState6 : ISolidState
+    {
+        public void Entering(object context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exiting(object context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
